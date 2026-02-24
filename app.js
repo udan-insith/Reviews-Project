3516da0e2e2da9cc97621235889e640b837b688e
udan-insith <udaninsith23613@gmail.com>
2026-02-24 05:59:13 +0530

    completed the project

--- app.js
+++ app.js
@@ -45,14 +45,38 @@ let currentItem = 0
 
 // load initial item
 window.addEventListener('DOMContentLoaded', function() {
-    showPerson(currentItem)
+    showPerson()
 })
 
 // show person based on item
-function showPerson(person) {
-    const item = reviews[person]
+function showPerson() {
+    const item = reviews[currentItem]
     img.src = item.img
     author.textContent = item.name
     job.textContent = item.job
     info.textContent = item.text
 }
+
+//show next person
+nextBtn.addEventListener('click', function() {
+    currentItem++
+    if(currentItem > reviews.length - 1) {
+        currentItem = 0
+    }
+    showPerson()
+})
+
+//show prev person
+prevBtn.addEventListener('click', function() {
+    currentItem--
+    if (currentItem < 0) {
+        currentItem = reviews.length - 1
+    }
+    showPerson()
+})
+
+// suprise me
+randomBtn.addEventListener('click', function() {
+    currentItem = Math.floor(Math.random() * reviews.length)
+    showPerson()
+})
